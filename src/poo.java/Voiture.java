d51d3c5d2cbd7fea2a65fc7583b588f3a040ff95
dikrabouhorma <dikra.bouhorma@gmail.com>
2025-03-11 12:02:02 +0100

    poo

---
+++ src/poo.java/Voiture.java
@@ -0,0 +1,12 @@
+
+public class Voiture {
+    public String marque;
+    public String modele;
+    public String couleur;
+
+    public void demarrer(){
+       System.out.println("la voiture a demarrer ! ");
+    }
+
+
+}
